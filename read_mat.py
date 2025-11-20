087fd9385c59094cb53aa2658c861d48e9b32bf3
tomas <tomyjara1@gmail.com>
2025-11-20 18:46:19 -0300

    refactor

--- read_mat.py
+++
@@ -1,6 +0,0 @@
-import pymatreader
-
-folder = "C:\\Users\\xochipilli\\Desktop\\DMT\\CleanEEG\\CleanEEG\\"
-
-bad_channels = pymatreader.read_mat(folder+"channels_removed.mat")
-rejected_epochs = pymatreader.read_mat(folder+"rejected_epochs.mat")
